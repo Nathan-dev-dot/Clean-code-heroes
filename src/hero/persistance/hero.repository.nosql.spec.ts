3057004ec213a19f7084b084d56fb9f15c21136a
nathan <maerna1@gmail.com>
2022-11-01 18:24:58 +0100

    find all hero database test

--- src/hero/persistance/hero.repository.nosql.spec.ts
+++ src/hero/persistance/hero.repository.nosql.spec.ts
@@ -2,9 +2,36 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { HeroRepositoryNosql } from './hero.repository.nosql';
 import { HeroController } from '../exposition/controller/hero.controller';
 import { HeroService } from '../application/hero.service';
+import { Hero } from '../domaine/hero';
+import { HeroRarities } from '../domaine/hero.rarities';
+import { HeroSpecialties } from '../domaine/hero.specialties';
 
 describe('HeroController', () => {
   let repository: HeroRepositoryNosql;
+  const mockedDatabase: Hero[] = [
+    new Hero({
+      id: '0',
+      armour: 20,
+      experiencePoints: 0,
+      healthPoints: 1000,
+      level: 0,
+      name: 'Nathan',
+      power: 100,
+      rarity: HeroRarities.Common,
+      specialty: HeroSpecialties.Tank,
+    }),
+    new Hero({
+      id: '1',
+      armour: 5,
+      experiencePoints: 0,
+      healthPoints: 800,
+      level: 0,
+      name: 'Sarah',
+      power: 150,
+      rarity: HeroRarities.Rare,
+      specialty: HeroSpecialties.Assassin,
+    }),
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,5 +44,10 @@ describe('HeroController', () => {
 
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  it('should be return all hero', async () => {
+    const heroes = await repository.findAll();
+    expect(heroes.length).toBe(2);
   });
 });
