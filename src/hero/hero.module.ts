f22213af007cfc887eb989128e48b670c69907a8
Nathan-dev-dot <maerna1@gmail.com>
2022-11-04 10:52:40 +0100

    update hero module with TypeOrm implementation

--- src/hero/hero.module.ts
+++ src/hero/hero.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { HeroService } from './application/hero.service';
 import { HeroController } from './exposition/controller/hero.controller';
 import { HeroRepositoryNosql } from './persistance/hero.repository.nosql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HeroEntity } from './entities/hero.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([HeroEntity])],
   controllers: [HeroController],
   providers: [HeroService, HeroRepositoryNosql],
 })
