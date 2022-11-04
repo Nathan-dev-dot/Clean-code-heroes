31317d1b8fd521e6197d29238467a4c9bc3f05fd
Nathan-dev-dot <maerna1@gmail.com>
2022-11-04 16:19:25 +0100

    fix testing module in hero controller spec

--- src/hero/exposition/controller/hero.controller.spec.ts
+++ src/hero/exposition/controller/hero.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HeroController } from './hero.controller';
 import { HeroService } from '../../application/hero.service';
+import { HeroRepositoryNosql } from '../../persistance/hero.repository.nosql';
 
 describe('HeroController', () => {
   let controller: HeroController;
@@ -8,7 +9,7 @@ describe('HeroController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HeroController],
-      providers: [HeroService],
+      providers: [HeroService, HeroRepositoryNosql],
     }).compile();
 
     controller = module.get<HeroController>(HeroController);
