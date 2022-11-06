7de611e40e23d1ee3a6aef93485dd34128cb1d33
Nathan-dev-dot <maerna1@gmail.com>
2022-11-06 15:20:37 +0100

    create hero exception filter to throw all http error and adapt http response

---
+++ src/hero/exposition/filter/hero.exception.filter.ts
@@ -0,0 +1,42 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HeroNotFoundException } from '../../application/exceptions/hero.not.found.exception';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HeroExceptionFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const body = {
+      statusCode:
+        exception instanceof HttpException
+          ? exception.getStatus()
+          : HttpStatus.INTERNAL_SERVER_ERROR,
+      timestamp: new Date().toISOString(),
+      message: exception.message,
+      path: request.url,
+    };
+
+    switch (exception.name) {
+      case HeroNotFoundException.name:
+        body.statusCode = 404;
+        break;
+      default:
+        if (exception! instanceof HttpException) {
+          console.log(
+            `Unhandled exception on '${request.url}' : '${exception.stack}' `,
+          );
+        }
+    }
+
+    response.status(body.statusCode).json(body);
+  }
+}
