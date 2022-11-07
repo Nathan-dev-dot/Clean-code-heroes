44810b498bedc249a2878fd29888baf6489a4ff5
Nathan-dev-dot <maerna1@gmail.com>
2022-11-07 20:43:55 +0100

    create hero entity

--- src/hero/entities/hero.entity.ts
+++ src/hero/entities/hero.entity.ts
@@ -1,7 +1,31 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'hero' })
 export class HeroEntity {
   @PrimaryGeneratedColumn()
   id: string;
+
+  @Column({ type: 'string', nullable: false })
+  name: string;
+
+  @Column({ type: 'integer', nullable: false })
+  healthPoints: number;
+
+  @Column({ type: 'integer', nullable: false })
+  experiencePoints: number;
+
+  @Column({ type: 'integer', nullable: false })
+  power: number;
+
+  @Column({ type: 'integer', nullable: false })
+  armour: number;
+
+  @Column({ type: 'string', nullable: false })
+  specialty: string;
+
+  @Column({ type: 'string', nullable: false })
+  rarity: string;
+
+  @Column({ type: 'integer', nullable: false })
+  level: number;
 }
