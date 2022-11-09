b236eaf19f13d64c85db89f22aae9cb17181d96e
Nathan-dev-dot <maerna1@gmail.com>
2022-11-09 09:42:01 +0100

    refactor object id transformer

--- src/utils/string.to.objectId.ts
+++ src/utils/string.to.objectId.ts
@@ -1,6 +1,5 @@
 import { ObjectID } from 'mongodb';
 
 export function strToObjectId(id: string): ObjectID {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  return require('mongodb').ObjectId(id);
+  return ObjectID(id);
 }
