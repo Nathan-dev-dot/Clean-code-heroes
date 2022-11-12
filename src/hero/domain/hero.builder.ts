36626744a938d024d8f485eee949b52b480a57ac
Nathan-dev-dot <maerna1@gmail.com>
2022-11-12 10:33:00 +0100

    create hero builder

---
+++ src/hero/domain/hero.builder.ts
@@ -0,0 +1,114 @@
+import { HeroSpecialties } from './hero.specialties';
+import { HeroRarities } from './hero.rarities';
+import { Hero } from './hero';
+import { HeroSpecificationBySpeciality } from './hero.specification.by.speciality';
+import { ErrorsCodesHero } from '../application/exceptions/errors.codes.hero';
+import { ObjectID } from 'mongodb';
+
+export class HeroBuilder {
+  private _id: ObjectID;
+  private _name: string = undefined;
+  private _healthPoints = 0;
+  private _experiencePoints = 0;
+  private _power = 0;
+  private _armour = 0;
+  private _specialty: HeroSpecialties = undefined;
+  private _rarity: HeroRarities = undefined;
+  private _level = 1;
+  private specifications = this.heroSpecifications.specifications();
+
+  constructor(private heroSpecifications: HeroSpecificationBySpeciality) {}
+
+  public id(id: ObjectID): HeroBuilder {
+    this._id = id;
+    return this;
+  }
+
+  public name(name: string): HeroBuilder {
+    this._name = name;
+    return this;
+  }
+
+  public specialty(speciality: string): HeroBuilder {
+    this._specialty = HeroSpecialties[speciality]
+      ? HeroSpecialties[speciality]
+      : undefined;
+    return this;
+  }
+
+  public setStandardHealthPointsPowerAndArmour(): HeroBuilder {
+    this.healthPoints(undefined);
+    this.power(undefined);
+    this.armour(undefined);
+    return this;
+  }
+
+  public healthPoints(healthPoints: number): HeroBuilder {
+    this._healthPoints = healthPoints
+      ? healthPoints
+      : this.specifications[this._specialty]
+      ? this.specifications[this._specialty].pv
+      : 0;
+    return this;
+  }
+
+  public power(power: number): HeroBuilder {
+    this._power = power
+      ? power
+      : this.specifications[this._specialty]
+      ? this.specifications[this._specialty].power
+      : 0;
+    return this;
+  }
+
+  public armour(armour: number): HeroBuilder {
+    this._armour = armour
+      ? armour
+      : this.specifications[this._specialty]
+      ? this.specifications[this._specialty].armour
+      : 0;
+    return this;
+  }
+
+  public experiencePoints(experiencePoints: number): HeroBuilder {
+    this._experiencePoints = experiencePoints;
+    return this;
+  }
+
+  public level(level: number): HeroBuilder {
+    this._level = level;
+    return this;
+  }
+
+  public rarity(rarity: string): HeroBuilder {
+    this._rarity = HeroRarities[rarity] ? HeroRarities[rarity] : undefined;
+    return this;
+  }
+
+  public build(): Hero | number {
+    if (this.checkHeroBuilder()) return ErrorsCodesHero.InvalidArgument;
+
+    return new Hero({
+      id: this._id ? this._id : undefined,
+      armour: this._armour,
+      experiencePoints: this._experiencePoints ? this._experiencePoints : 1,
+      healthPoints: this._healthPoints,
+      level: this._level ? this._level : 1,
+      name: this._name,
+      power: this._power,
+      rarity: this._rarity,
+      specialty: this._specialty,
+    });
+  }
+
+  private checkHeroBuilder() {
+    return (
+      this._rarity == undefined ||
+      this._specialty == undefined ||
+      this._healthPoints <= 0 ||
+      this._power <= 0 ||
+      this._level <= 0 ||
+      this._armour <= 0
+    );
+  }
+}
